568de6d16de14a5525cea3228e6ad563d3e9a881
orltrh <aurelyataroreh@gmail.com>
2022-09-17 22:01:17 +0700

    lecture3

---
+++ lecture3.py
@@ -0,0 +1,16 @@
+#get user input
+answer = input("Greeting: ")
+
+new_answer = answer.lower().strip()
+
+#check if the answer has 'hello', print 0
+if 'hello' in new_answer:
+    print("$0")
+
+#cehck if answer starts with 'h', print 20
+elif 'how you doing?' == new_answer[0]:
+    print("$20")
+
+#otherwise, print 100
+else:
+    print("$100")
